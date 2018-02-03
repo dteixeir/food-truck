8fb9e3dfbdda0738800ccb122408c41efac03ef5
Danny Teixeira <danotex1128@gmail.com>
2018-02-03 10:56:59 -0500

    incorporated in memory db provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './/app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
-import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './appComponent/app.component';
 import { DashboardComponent } from './dashboard/dashboard';
 
-import { HeroesComponent } from './hero-master/hero-master.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './hero-master/hero-master.component';
 import { MessagesComponent } from './messages/messages';
 
 import { HeroService, MessageService } from './services';
-
 
 @NgModule({
   declarations: [
@@ -28,9 +29,10 @@ import { HeroService, MessageService } from './services';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule, ,
+    HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
+      InMemoryDataService,
+      { dataEncapsulation: false }
     )
   ],
   providers: [
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -13,6 +13,10 @@ export class HeroService {
 
   private heroesUrl = 'api/heroes';  // URL to web api
 
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
   constructor(
     private http: HttpClient,
     private messageService: MessageService
@@ -26,12 +30,20 @@ export class HeroService {
       );
   }
 
-  getHero(id): Observable<Hero> {
-    return this.http.get<Hero>(`this.heroesUrl/${id}`)
+  getHero(id: number): Observable<Hero> {
+    return this.http.get<Hero>(`${this.heroesUrl}/${id}`)
       .pipe(
         tap(_ => this.log(`fetched hero id=${id}`)),
         catchError(this.handleError<Hero>(`getHero id=${id}`))
       );
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero (hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
   }
 
   private log(message: string) {
